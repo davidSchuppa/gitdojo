129f0da2032f18ea2cefb12737cc9d6cdf1406c9
dschuppa <d.schuppa@gmail.com>
2018-05-03 11:11:04 +0200

    pushed stash changes

--- gittie.py
+++ gittie.py
@@ -3,10 +3,14 @@ def introduce():
 
 def add(a,b):
     print(a + b)
-    
+
 def joke():
     print("""What happens when you witness a ship wreck?
 You let it sink in.""")
 
 def shout():
+<<<<<<< Updated upstream
     print("I'M NOW SHOUTING!")
+=======
+    print("WHAAAAAAAAAAAAAAAAAAT")
+>>>>>>> Stashed changes
